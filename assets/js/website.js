ec6798e11d6f6c92d31c4b20abe3de4f7b4653be
Kahina Ouali <kahinaouali.pro@gmail.com>
2024-07-29 18:39:27 +0200

    Ajout de la section services

--- assets/js/website.js
+++ assets/js/website.js
@@ -5,3 +5,15 @@ const btnMenu = document.querySelector("#button-menu");
 btnMenu.addEventListener("click", () => {
     btnMenu.classList.toggle("active");
 });
+
+const sidebar = document.getElementById("sidebar");
+const toggleBtn = document.getElementById("button-menu");
+
+const toggleSidebar = () => {
+
+    const isClosed = sidebar.style.width === '0px' || !sidebar.style.width;
+    sidebar.style.width = isClosed ? '380px' : '0px';
+
+};
+
+toggleBtn.addEventListener("click", toggleSidebar);
